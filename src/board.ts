11826a61fe2e32e7648a7ff5b421e0cafc2f3701
vlad151 <lazar.vlad151@gmail.com>
2023-10-08 21:15:19 +0200

    board class

--- src/board.ts
+++ src/board.ts
@@ -1,3 +1,4 @@
+
 export type Generator<T>= { next:() => T } 
 
 export type Position = {
@@ -10,31 +11,73 @@ export type Match<T> = {
     positions: Position[]
 }
 
-export type BoardEvent<T> = any;
+export type BoardEvent<T> = {
+    type: "move",
+    first: Position,
+    second: Position,
+    match: Match<T>
+}
 
-export type BoardListener<T> = any;
+export type BoardListener<T> = (event: BoardEvent<T>) => void;
 
 export class Board<T> {
     readonly width: number
     readonly height: number
+    private board: T[][]
 
     constructor(generator: Generator<T>, width: number, height: number) {
+        
         this.width = width
         this.height = height
+        this.board = []
+
+        for (let i = 0; i < height; i++) {
+            const row: T[] = [];
+            for (let j = 0; j < width; j++) {
+              row.push(generator.next());
+            }
+            this.board.push(row);
+          }
     }
 
     addListener(listener: BoardListener<T>) {
+            
+        
     }
 
-    positions(): any{
+    positions(): Position[]{
+        const positions: Position[] = [];
+        for (let row = 0; row < this.height; row++) {
+          for (let col = 0; col < this.width; col++) {
+            positions.push({ row, col });
+          }
+        }
+        return positions;
     }
 
-    piece(p: Position): any{
-    }
+    piece(p: Position): T | undefined {
+        if (this.isValidPosition(p)) {
+          return this.board[p.row][p.col];
+        }
+        return undefined;
+      }
 
-    canMove(first: Position, second: Position):any{
-    }
+      private isValidPosition(p: Position): boolean {
+        return p.row >= 0 && p.row < this.height && p.col >= 0 && p.col < this.width;
+      }
+
+
+      canMove(first: Position, second: Position): boolean {
+        if (!this.isValidPosition(first) || !this.isValidPosition(second)) {
+          return false;
+        }}
     
-    move(first: Position, second: Position) {
+     move(first: Position, second: Position) {
+    if (this.canMove(first, second)) {
+      const temp = this.board[first.row][first.col];
+      this.board[first.row][first.col] = this.board[second.row][second.col];
+      this.board[second.row][second.col] = temp;
+
     }
+  }
 }
